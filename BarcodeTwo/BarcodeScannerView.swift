d391971a63318dbdb18ec31940f0c364e91aaa28
Dan Payne <dpayne5532@gmail.com>
2022-01-28 05:20:58 -0700

    Initial Commit

---
+++ BarcodeTwo/BarcodeScannerView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarcodeTwo
+//
+//  Created by Dan Payne on 1/28/22.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeScannerView()
+    }
+}
---
+++ BarcodeTwo/BarcodeTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeTwoApp.swift
+//  BarcodeTwo
+//
+//  Created by Dan Payne on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
